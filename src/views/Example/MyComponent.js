345a5250a5e6ebf8bed133c50a97278e8ccb869a
unknown <you@example.com>
2025-02-12 15:55:12 +0700

    class component

---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+    render() {
+        return <div>Hello my component Hoi dan IT</div>;
+    }
+}
+
+export default MyComponent;
