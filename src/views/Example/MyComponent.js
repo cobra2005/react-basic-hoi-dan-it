f7c61611f42f26cef39a55a0934ad116b0f19adb
unknown <you@example.com>
2025-02-14 05:47:28 +0700

    learn state

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+    state = {
+        name: 'Thai',
+        university: 'PTIT',
+    };
+
+    handleOnChangeName(event) {
+        return this.setState({
+            name: event.target.value,
+        });
+    }
+
     render() {
-        return <div>Hello my component Hoi dan IT</div>;
+        return (
+            <>
+                <input
+                    value={this.state.name}
+                    type="text"
+                    onChange={(event) => this.handleOnChangeName(event)}
+                />
+                <p>
+                    My name is {this.state.name}, I'm learning at{' '}
+                    {this.state.university}
+                </p>
+            </>
+        );
     }
 }
 
